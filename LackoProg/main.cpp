7281cafa984bf03e8d6e4f192907b47108d044e1
Oliver Kocsis <oliver.kocsis@imind.eu>
2015-09-26 19:42:52 +0200

    Initial Commit

---
+++ LackoProg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LackoProg
+//
+//  Created by Kocsis Olivér on 2015. 09. 26..
+//  Copyright © 2015. okocsis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
