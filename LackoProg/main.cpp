df8780c8935ba64b0df1dd3764214948d493450a
kocsislaci <kocsislaci95@gmail.com>
2015-09-27 22:03:24 +0200

    per ennek

--- LackoProg/main.cpp
+++ LackoProg/main.cpp
@@ -13,7 +13,6 @@
 
 #define scanf_s(args...) scanf(args)
 
-
 typedef unsigned char byte;
 
 int main(int argc, const char * argv[]) {
@@ -23,107 +22,107 @@ int main(int argc, const char * argv[]) {
     
     
     
-//    z=0;
-//    int z;
-//    printf("irj be egy syamot ");
-//    scanf_s("%i", %z);
-//    if (z == 0)
-//    printf("n-t irtal. \n");
-//    else
-//        printf("nem 0t irtal \n");
-//    printf("z=%i\n", z);
-//    
-//    int i = 9;
-//    int j = 10;
-//    double x =(double)i / j;
-//    printf("%i/%i=%g\n", i, j, x);
-//    
-//    int h7;
-//    printf("irj be egy szamot ");
-//           scanf_s("%i", &h7);
-//           if (h7 % 7) printf("nem ");
-//               printf("oszthato 7-tel\n");
-//
+    //    z=0;
+    //    int z;
+    //    printf("irj be egy syamot ");
+    //    scanf_s("%i", %z);
+    //    if (z == 0)
+    //    printf("n-t irtal. \n");
+    //    else
+    //        printf("nem 0t irtal \n");
+    //    printf("z=%i\n", z);
+    //
+    //    int i = 9;
+    //    int j = 10;
+    //    double x =(double)i / j;
+    //    printf("%i/%i=%g\n", i, j, x);
+    //
+    //    int h7;
+    //    printf("irj be egy szamot ");
+    //           scanf_s("%i", &h7);
+    //           if (h7 % 7) printf("nem ");
+    //               printf("oszthato 7-tel\n");
+    //
     
-//    unsigned char c = 0;
-//    unsigned char allTrue = (1 << 8) - 1  ;
-//    
-//    unsigned char allTrue2 = 1 | 2 | 4 | 8 | 16 | 32 | 64 | 128;
-    
-    
-    
-//    for (int i = 0; i < 5000; ++i) {
-//        
-//        
-//        printf("%i\n",c++);
-//        
-//
-//    }
-    
-    
-//    if (false && ) {
-//        printf("ezt megusztuk \n");
-//    }
+    //    unsigned char c = 0;
+    //    unsigned char allTrue = (1 << 8) - 1  ;
+    //
+    //    unsigned char allTrue2 = 1 | 2 | 4 | 8 | 16 | 32 | 64 | 128;
     
     
     
-//    int szam = 0;
-//    
-//    szam += 5; // eqvivalens
-//    szam = szam + 5; //
-//    
-//           // bitvarazslat
-//    
-//    byte acc = 16;
-//    int bitszam;
-//    while (true) {
-//
-//        
-//        printf("acc=%i, hanyadik bit set? ", acc);
-//        scanf_s("%i", &bitszam);
-//        acc |= (1 << bitszam);
-//        printf("acc=%i, vagyis 0x%x\n", acc, acc);
-//        
-//        
-//        if (bitszam == 8) {
-//            break;
-//        }
-//        
-//    }
-//    
-//    // uj programres
-//    
-//    byte acc2 = 16;
-//    int bitszam2;
-//    
-//    printf(" \n ++++ uj programres ++++\n");
-//    acc2 = 0xff;
-//    while (true) {
-//        
-//        
-//        printf("acc=%i, hanyadik bit reset ? ", acc2);
-//        scanf_s("%i", &bitszam2);
-//        acc2 = acc2 & (~ (1 << bitszam2));
-//        printf("acc=%i, vagyis 0x%x\n", acc2, acc2);
-//        
-//        
-//        if (bitszam2 == 8) {
-//            break;
-//        }
-//    }
-//    
-//    
-//
+    //    for (int i = 0; i < 5000; ++i) {
+    //
+    //
+    //        printf("%i\n",c++);
+    //
+    //
+    //    }
     
-     
-           
-           
-//    
-//    double a, b, c, d, x1, x2;
-//    printf("masodfoku egyenlet...\n");
-//    printf("a:\nb:\nc:");
-//    
-//    scanf_s("%lf", &a);
+    
+    //    if (false && ) {
+    //        printf("ezt megusztuk \n");
+    //    }
+    
+    
+    
+    //    int szam = 0;
+    //
+    //    szam += 5; // eqvivalens
+    //    szam = szam + 5; //
+    //
+    //           // bitvarazslat
+    //
+    //    byte acc = 16;
+    //    int bitszam;
+    //    while (true) {
+    //
+    //
+    //        printf("acc=%i, hanyadik bit set? ", acc);
+    //        scanf_s("%i", &bitszam);
+    //        acc |= (1 << bitszam);
+    //        printf("acc=%i, vagyis 0x%x\n", acc, acc);
+    //
+    //
+    //        if (bitszam == 8) {
+    //            break;
+    //        }
+    //
+    //    }
+    //
+    //    // uj programres
+    //
+    //    byte acc2 = 16;
+    //    int bitszam2;
+    //
+    //    printf(" \n ++++ uj programres ++++\n");
+    //    acc2 = 0xff;
+    //    while (true) {
+    //
+    //
+    //        printf("acc=%i, hanyadik bit reset ? ", acc2);
+    //        scanf_s("%i", &bitszam2);
+    //        acc2 = acc2 & (~ (1 << bitszam2));
+    //        printf("acc=%i, vagyis 0x%x\n", acc2, acc2);
+    //
+    //
+    //        if (bitszam2 == 8) {
+    //            break;
+    //        }
+    //    }
+    //
+    //
+    //
+    
+    
+    
+    
+    //
+    //    double a, b, c, d, x1, x2;
+    //    printf("masodfoku egyenlet...\n");
+    //    printf("a:\nb:\nc:");
+    //
+    //    scanf_s("%lf", &a);
     
     
     
@@ -146,9 +145,10 @@ int main(int argc, const char * argv[]) {
     cy = az * bx - ax * bz;
     cz = ax * by - ay * bx;
     
-    printf("cx=%lf", cx);
-    printf("cy=%lf", cy);
-    printf("cz=%lf", cy);
+    printf("cx= %lf\n", cx);
+    printf("cy= %lf\n", cy);
+    printf("cz= %lf\n", cy);
+    printf("juhuhu");
     
     
     
